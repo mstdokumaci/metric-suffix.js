24dd4eafbb51c29f593417df137c9e35ebaab5d9
Mustafa Dokumacı <mustafa@foraa.com>
2015-12-10 15:36:26 +0200

    bug fixes

--- metric-suffix.js
+++ metric-suffix.js
@@ -2,6 +2,7 @@
 	'use strict';
 
 	function metric_suffix (number, digits) {
+		if (number < 0) return 0;
 		if (number < 1e3) return number;
 		
 		if (!digits) digits = 2;
@@ -12,7 +13,7 @@
 		var exp2 = Math.ceil(Math.log(number) / Math.log(1e1));
 		if (digits < exp2) digits = exp2;
 		
-		return number.toPrecision(digits) + 'kMGTPE'.charAt(exp - 1);
+		return +number.toPrecision(digits) + 'kMGTPE'.charAt(exp - 1);
 	}
 
 	if (environment.exports)
